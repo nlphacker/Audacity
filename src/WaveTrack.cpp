nlphacker/Audacity

commit 75cb95ea3182eaf9670efdef4e6ff408814a81c1

See http://bugzilla.audacityteam.org/show_bug.cgi?id=451#c31 and http://bugzilla.audacityteam.org/show_bug.cgi?id=451#c32.
git-svn-id: http://audacity.googlecode.com/svn/audacity-src/trunk@11308 47890b92-0858-11df-a26f-8b716316a5bc

src/WaveTrack.cpp
@@ -576,7 +576,7 @@ bool WaveTrack::ClearAndPaste(double t0, // Start of time to clear
                // falls within it and this isn't the first clip in the track.
                if (fabs(t1 - clip->GetStartTime()) < WAVETRACK_MERGE_POINT_TOLERANCE) {
                   if (i > 0) {
-                     MergeClips(GetClipIndex(clips[i - 1]), GetClipIndex(clip));
+                     wxASSERT(MergeClips(GetClipIndex(clips[i - 1]), GetClipIndex(clip)));
                   }
                   break;
                }
@@ -594,7 +594,7 @@ bool WaveTrack::ClearAndPaste(double t0, // Start of time to clear
                // falls within it and this isn't the last clip in the track.
                if (fabs(t0 - clip->GetEndTime()) < WAVETRACK_MERGE_POINT_TOLERANCE) {
                   if (i < clips.GetCount() - 1) {
-                     MergeClips(GetClipIndex(clip), GetClipIndex(clips[i + 1]));
+                     wxASSERT(MergeClips(GetClipIndex(clip), GetClipIndex(clips[i + 1])));
                   }
                   break;
                }
@@ -638,8 +638,7 @@ bool WaveTrack::ClearAndPaste(double t0, // Start of time to clear
    }
 
    // If we created a default time warper, we need to delete it
-   if (effectWarper == NULL)
-      delete warper;
+   delete warper;
 
    return true;
 }
@@ -814,6 +813,7 @@ bool WaveTrack::SyncLockAdjust(double oldT1, double newT1)
             if (!ret) return false;
 
             ret = Paste(newT1, tmp);
+            wxASSERT(ret);
             delete tmp;
          }
 
@@ -828,9 +828,9 @@ bool WaveTrack::SyncLockAdjust(double oldT1, double newT1)
          if (!f) return false;
          WaveTrack *tmp = f->NewWaveTrack(GetSampleFormat(), GetRate());
 
-         tmp->InsertSilence(0.0, newT1 - oldT1);
+         wxASSERT(tmp->InsertSilence(0.0, newT1 - oldT1));
          tmp->Flush();
-         Paste(oldT1, tmp);
+         wxASSERT(Paste(oldT1, tmp));
          delete tmp;
       }
    }
@@ -846,18 +846,13 @@ bool WaveTrack::Paste(double t0, Track *src)
 {
    bool editClipCanMove = true;
    gPrefs->Read(wxT("/GUI/EditClipCanMove"), &editClipCanMove);
-   
-   //printf("paste: entering WaveTrack::Paste\n");
-   
-   // JKC Added...
+
    if( src == NULL )
       return false;
 
    if (src->GetKind() != Track::Wave)
       return false;
       
-   //printf("paste: we have a wave track\n");
-
    WaveTrack* other = (WaveTrack*)src;
    
    //
@@ -902,7 +897,7 @@ bool WaveTrack::Paste(double t0, Track *src)
          if (!IsEmpty(t0, GetEndTime())) {
             Track *tmp = NULL;
             Cut(t0, GetEndTime()+1.0/mRate, &tmp);
-            Paste(t0 + insertDuration, tmp);
+            wxASSERT(Paste(t0 + insertDuration, tmp));
             delete tmp;
          }
       } else
@@ -1206,12 +1201,12 @@ bool WaveTrack::Join(double t0, double t1)
       if (clip->GetOffset() - t > (1.0 / mRate)) {
          double addedSilence = (clip->GetOffset() - t);
          //printf("Adding %.6f seconds of silence\n");
-         newClip->InsertSilence(t, addedSilence);
+         wxASSERT(newClip->InsertSilence(t, addedSilence));
          t += addedSilence;
       }
 
       //printf("Pasting at %.6f\n", t);
-      newClip->Paste(t, clip);
+      wxASSERT(newClip->Paste(t, clip));
       t = newClip->GetEndTime();      
 
       mClips.DeleteObject(clip);
@@ -2114,6 +2109,7 @@ bool WaveTrack::MergeClips(int clipidx1, int clipidx2)
    WaveClip* clip1 = GetClipByIndex(clipidx1);
    WaveClip* clip2 = GetClipByIndex(clipidx2);
 
+   wxASSERT(clip2);
    if(clip2 == NULL) // could happen if one track of a linked pair had a split and the other didn't
       return false;
    

src/AudacityApp.cpp
@@ -1577,7 +1577,7 @@ bool AudacityApp::CreateSingleInstanceChecker(wxString dir)
          runningTwoCopiesStr +
          _("Do you still want to start Audacity?");
       int action = wxMessageBox(prompt,
-                                _("Error locking temporary folder"),
+                                _("Error Locking Temporary Folder"),
                                 wxYES_NO | wxICON_EXCLAMATION,
                                 NULL);
       if (action == wxNO) {

src/AudioIO.cpp
@@ -1754,7 +1754,7 @@ void AudioIO::StopStream()
                      if (recordingOffset < 0)
                         track->Clear(mT0, mT0 - recordingOffset); // cut the latency out
                      else
-                        track->InsertSilence(mT0, recordingOffset); // put silence in
+                        wxASSERT(track->InsertSilence(mT0, recordingOffset)); // put silence in
                   }
                   else
                   {  // recording into a new track

src/Benchmark.cpp
@@ -423,7 +423,11 @@ void BenchmarkDialog::OnRun( wxCommandEvent &event )
       if (mEditDetail)
          Printf(wxT("Paste: %d\n"), y0 * chunkSize);
 
-      t->Paste(double (y0 * chunkSize), tmp);
+      if (!t->Paste((double)(y0 * chunkSize), tmp))
+      {
+         Printf(wxT("Trial %d\nFailed on Paste.\n"), z);
+         goto fail;
+      }
 
       if (t->GetClipByIndex(0)->GetSequence()->GetNumSamples() != (sampleCount) nChunks * chunkSize) {
          Printf(wxT("Trial %d\n"), z);

src/Menus.cpp
@@ -3465,7 +3465,7 @@ void AudacityProject::OnPaste()
             }else{
                WaveTrack *tmp;
                tmp = mTrackFactory->NewWaveTrack( ((WaveTrack*)n)->GetSampleFormat(), ((WaveTrack*)n)->GetRate());
-               tmp->InsertSilence(0.0, msClipT1 - msClipT0); // MJS: Is this correct?
+               wxASSERT(tmp->InsertSilence(0.0, msClipT1 - msClipT0)); // MJS: Is this correct?
                tmp->Flush();
 
                bPastedSomething |= 
@@ -3590,19 +3590,16 @@ bool AudacityProject::HandlePasteNothingSelected()
             pNewTrack = mTrackFactory->NewTimeTrack();
             break;
          default:
-            // Vaughan, 2010-08-05: 
-            //    This is probably an error, but was never checked...
-            //    The only kinds of tracks not checked above are Track::None and Track::All. 
             pClip = iterClip.Next();
             continue;
          }
+         wxASSERT(pClip);
 
          pNewTrack->SetLinked(pClip->GetLinked());
          pNewTrack->SetChannel(pClip->GetChannel());
          pNewTrack->SetName(pClip->GetName());
 
-         // Vaughan, 2010-08-05: This code never checked the paste result...
-         pNewTrack->Paste(0.0, pClip); 
+         wxASSERT(pNewTrack->Paste(0.0, pClip)); 
          mTracks->Add(pNewTrack);
          pNewTrack->SetSelected(true);         
          

src/Project.cpp
@@ -2328,7 +2328,7 @@ void AudacityProject::OpenFile(wxString fileName, bool addtohistory)
    int numRead = ff->Read(buf, 15);
    if (numRead != 15) {
       wxMessageBox(wxString::Format(_("File may be invalid or corrupted: \n%s"), 
-                   (const wxChar*)fileName), _("Error opening file or project"),
+                   (const wxChar*)fileName), _("Error Opening File or Project"),
                    wxOK | wxCENTRE, this);
      ff->Close();
      delete ff;
@@ -3043,7 +3043,7 @@ bool AudacityProject::Save(bool overwrite /* = true */ ,
       {
          if (mUndoManager.UnsavedChanges() && mEmptyCanBeDirty) {
             int result = wxMessageBox(_("Your project is now empty.\nIf saved, the project will have no tracks.\n\nTo save any previously open tracks:\nClick 'No', Edit > Undo until all tracks\nare open, then File > Save Project.\n\nSave anyway?"),
-                                      _("Warning empty project"),
+                                      _("Warning - Empty Project"),
                                       wxYES_NO | wxICON_QUESTION, this);
             if (result == wxNO)
                return false;
@@ -3116,7 +3116,7 @@ bool AudacityProject::Save(bool overwrite /* = true */ ,
          wxMessageBox(wxString::Format(
             _("Could not save project. Path not found.  Try creating \ndirectory \"%s\" before saving project with this name."),
             projPath.c_str()),
-                      _("Error saving project"),
+                      _("Error Saving Project"),
                       wxICON_ERROR, this);
          return false;
       }
@@ -3153,7 +3153,7 @@ bool AudacityProject::Save(bool overwrite /* = true */ ,
             wxRename(safetyFileName, mFileName);
          wxMessageBox(wxString::Format(_("Could not save project. Perhaps %s \nis not writable or the disk is full."),
                                        project.c_str()),
-                      _("Error saving project"),
+                      _("Error Saving Project"),
                       wxICON_ERROR, this);
          return false;
       }
@@ -3176,7 +3176,7 @@ bool AudacityProject::Save(bool overwrite /* = true */ ,
       wxMessageBox(wxString::Format(
          _("Couldn't write to file \"%s\": %s"),
          mFileName.c_str(), pException->GetMessage().c_str()),
-         _("Error saving project"), wxICON_ERROR);
+         _("Error Saving Project"), wxICON_ERROR);
 
       delete pException;
 
@@ -3443,10 +3443,10 @@ bool AudacityProject::Import(wxString fileName, WaveTrackArray* pTrackArray /*= 
 
    if (!errorMessage.IsEmpty()) {
 // Version that goes to internet...
-//      ShowErrorDialog(this, _("Error importing"),
+//      ShowErrorDialog(this, _("Error Importing"),
 //                 errorMessage, wxT("http://audacity.sourceforge.net/help/faq?s=files&i=wma-proprietary"));   
 // Version that looks locally for the text.
-      ShowErrorDialog(this, _("Error importing"),
+      ShowErrorDialog(this, _("Error Importing"),
                  errorMessage, wxT("innerlink:wma-proprietary"));   
    }
    if (numTracks <= 0)
@@ -3539,7 +3539,11 @@ bool AudacityProject::SaveAs(bool bWantSaveCompressed /*= false*/)
    //We should only overwrite it if this project already has the same name, where the user
    //simply chose to use the save as command although the save command would have the effect.
    if(mFileName!=fName+ext && wxFileExists(fName+ext)) {
-      wxMessageDialog m(NULL, _("The project was not saved because the file name provided would overwrite another project.\nPlease try again and select an original name."), _("Error Saving Project"), wxOK|wxICON_ERROR);
+      wxMessageDialog m(
+         NULL, 
+         _("The project was not saved because the file name provided would overwrite another project.\nPlease try again and select an original name."), 
+         _("Error Saving Project"), 
+         wxOK|wxICON_ERROR);
       m.ShowModal();
       return false;
    }
@@ -4190,7 +4194,7 @@ void AudacityProject::EditClipboardByLabel( WaveTrack::EditDestFunction action )
                            regions.Item(i+1)->start - regions.Item(i)->end);
                   }
 
-                  merged->Paste( 0.0 , dest );
+                  wxASSERT(merged->Paste( 0.0 , dest ));
                   delete dest;
                }
             }
@@ -4392,7 +4396,7 @@ void AudacityProject::AutoSave()
       wxMessageBox(wxString::Format(
          _("Couldn't write to file \"%s\": %s"),
          (fn + wxT(".tmp")).c_str(), pException->GetMessage().c_str()),
-         _("Error writing autosave file"), wxICON_ERROR, this);
+         _("Error Writing Autosave File"), wxICON_ERROR, this);
 
       delete pException;
 

src/Sequence.cpp
@@ -80,7 +80,7 @@ Sequence::Sequence(const Sequence &orig, DirManager *projDirManager)
 
    mBlock = new BlockArray();
 
-   Paste(0, &orig);
+   wxASSERT(Paste(0, &orig));
 }
 
 Sequence::~Sequence()
@@ -533,6 +533,7 @@ bool Sequence::Paste(sampleCount s, const Sequence *src)
          insertBlock->f = mDirManager->CopyBlockFile(srcBlock->Item(i)->f);
          if (!insertBlock->f) {
             // TODO error: Could not paste!  (Out of disk space?)
+            wxASSERT(false);
             return false;
          }
 
@@ -622,6 +623,7 @@ bool Sequence::InsertSilence(sampleCount s0, sampleCount len)
    sTrack->mNumSamples = pos;
 
    bool bResult = Paste(s0, sTrack);
+   wxASSERT(bResult);
 
    delete sTrack;
 
@@ -936,7 +938,7 @@ void Sequence::WriteXML(XMLWriter &xmlFile)
                _("Sequence has block file with length %s > mMaxSamples %s.\nTruncating to mMaxSamples."), 
                Internat::ToString(((wxLongLong)(bb->f->GetLength())).ToDouble(), 0).c_str(), 
                Internat::ToString(((wxLongLong)mMaxSamples).ToDouble(), 0).c_str());
-         ::wxMessageBox(sMsg, _("Warning Writing Sequence"), wxICON_EXCLAMATION | wxOK);
+         ::wxMessageBox(sMsg, _("Warning - Length in Writing Sequence"), wxICON_EXCLAMATION | wxOK);
          ::wxLogWarning(sMsg);
          bb->f->SetLength(mMaxSamples);
       }

src/Tags.cpp
@@ -1086,7 +1086,7 @@ void TagsEditor::OnLoad(wxCommandEvent & event)
    if (!reader.Parse(&mLocal, fn)) {
       // Inform user of load failure
       wxMessageBox(reader.GetErrorStr(),
-                   _("Error loading metadata"),
+                   _("Error Loading Metadata"),
                    wxOK | wxCENTRE,
                    this);
    }
@@ -1170,7 +1170,7 @@ void TagsEditor::OnSave(wxCommandEvent & event)
       wxMessageBox(wxString::Format(
          _("Couldn't write to file \"%s\": %s"),
          fn.c_str(), pException->GetMessage().c_str()),
-         _("Error saving tags file"), wxICON_ERROR, this);
+         _("Error Saving Tags File"), wxICON_ERROR, this);
 
       delete pException;
    }

commit 0cc8733a649dbb6e544de11e6cdb7320b428ff1b

Fix memory leak I introduced in r11308.

src/WaveTrack.cpp
@@ -638,7 +638,8 @@ bool WaveTrack::ClearAndPaste(double t0, // Start of time to clear
    }
 
    // If we created a default time warper, we need to delete it
-   delete warper;
+   if (effectWarper == NULL)
+      delete warper;
 
    return true;
 }